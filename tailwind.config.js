8aa7ac96529828006659b23c521a4f8492381a40
David Soards <davidysoards@gmail.com>
2020-10-02 18:55:48 -0400

    Update Tailwind PostCSS integrations and all deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  experimental: {
+    applyComplexClasses: true,
+  },
+  purge: {
+    // enabled: true,
+    content: ['./src/site/**/*.njk'],
+  },
   theme: {
     fontFamily: {
       sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
@@ -9,12 +27,13 @@ module.exports = {
       center: true,
     },
     extend: {
-      colors: {
-        brand: '#cb1d1f',
-        base: '#fafafa',
-      },
+      // screens: {
+      //   2xl: '1536px',
+      // },
     },
   },
-  variants: {},
+  variants: {
+    // opacity: ({ after }) => after(['disabled']),
+  },
   plugins: [],
 };
